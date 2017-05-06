0981ae548df0a523b08579d673c4f69160ad765c
Vikas K <vikasjkitawat@gmail.com>
2017-05-06 16:59:17 +0530

    Added Database Project and the tables Users and Login

---
+++ ViKi.NetMVCBoilerPlate/ViKi.NetMVCBoilerPlate.Database/Login.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Login]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserID] NVARCHAR(100) NOT NULL, 
+    [Username] NVARCHAR(100) NOT NULL, 
+    [Password] NVARCHAR(200) NULL, 
+    [HashKey] NVARCHAR(200) NULL, 
+    [Tries] INT NULL
+)
---
+++ ViKi.NetMVCBoilerPlate/ViKi.NetMVCBoilerPlate.Database/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] NVARCHAR(100) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(100) NULL, 
+    [Phone] NVARCHAR(50) NULL, 
+    [Active] BIT NULL
+)
